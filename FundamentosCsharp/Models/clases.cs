598af65ee788fb09e472a153cb8638d66e02ca6b
JuanPablo-Sepulveda <juanchuten8@gmail.com>
2025-03-20 18:20:22 -0300

    c# Parte 2

---
+++ FundamentosCsharp/Models/clases.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosCsharp.Models
+{
+    internal class Bebida
+    {
+        public string Nombre { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public Bebida(string Nombre, int Cantidad)// el constructor recibe los parametros que queramos modificar y su funcion a realizar
+        {
+            this.Nombre= Nombre;// declaramos que el nombre que recibamos en el parametro nombre va a ser nuestra nueva bebida
+            this.Cantidad = Cantidad;
+        }
+
+        public void Beberse(int CuantoBebio)
+        {
+            this.Cantidad -= CuantoBebio;
+        }
+    }
+}
---
+++ FundamentosCsharp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FundamentosCsharp.Models;// añadimos un models del directorio clases.cs 
+
+
+
+internal class Program()
+{
+    static void Main(string[]args)
+    {
+        //clases,objetos y constructores
+        Bebida bebida = new Bebida("Coca cola",1000);
+        bebida.Beberse(500);
+       
+        Console.WriteLine(bebida.Cantidad);
+
+        //Listas y Arreglos
+        int[] numeros = new int[10] {1,2,3,4,5,6,7,8,9,0};
+
+        for(int i=0; i<numeros.Length /*condicion*/; i++ /*indicamos el cambiio*/)// metodo For
+        {
+            Console.WriteLine("iteracion: "+i+"-"+numeros[i]);
+        }
+        foreach(var numero in numeros)// metodo ForEach
+        {
+            Console.WriteLine(numero);
+        }
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("LISTA");
+
+        List<int> lista = new List<int>() { 1,2,3,4,5,6,7,8};
+        lista.Add(9);
+        lista.Add(0);
+        
+
+        foreach(var numero in lista)
+        {
+            Console.WriteLine("Elemento: "+numero);
+        }
+
+        List<Cerveza> cervezas = new List<Cerveza>(){new Cerveza(10,"cerveza premium")};
+        cervezas.Add(new Cerveza(500));
+        Cerveza brahama = new Cerveza(500," cerveza de cebada");
+        cervezas.Add(brahama);
+
+        foreach (var cervezass in cervezas)
+        {
+            Console.WriteLine("cerveza: "+cervezass.Nombre);
+        }
+        Console.WritwLine("*******************************************);
+        Console.WriteLine("Interfaces");
+
+    }
+}
